4e41f34b1531a800a15ab270becf701b1515c352
= <=>
2023-10-02 20:56:22 -0300

    Finalizado

--- src/routes.js
+++ src/routes.js
@@ -10,23 +10,15 @@ const rotas = express();
 
 //http://localhost:3000/contas?senha_banco=Cubos123Bank
 rotas.get('/contas', validarSenha, listarContas);
-//http://localhost:3000/contas
 rotas.post('/contas', verificarCampos, emailCpfExiste, criarConta);
-//http:/localhost:3000/contas/:numeroConta/usuario
 rotas.put('/contas/:numeroConta/usuario', verificarCampos, emailCpfExiste, verificarNumeroContaParams, verificarContaParams, atualizarUsuario);
-//http://localhost:3000/:numeroConta
 rotas.delete('/contas/:numeroConta',verificarContaParams, excluirConta);
 
-//http://localhost:3000/contas/saldo/numero_conta
 rotas.get('/contas/saldo', verificarNumeroContaQuery, verificarSenhaQuery, saldo);
-//http://localhost:3000/contas/extrato?numero_conta
 rotas.get('/contas/extrato',verificarNumeroContaQuery, verificarSenhaQuery, extrato);
 
-//http://localhost:3000/transacoes/depositar
 rotas.post('/transacoes/depositar', depositar);
-//http://localhost:3000/transacoes/sacar
 rotas.post('/transacoes/sacar', sacar);
-//http://localhost:3000/transacoes/transferir
 rotas.post('/transacoes/transferir', transferir);
 
 module.exports = rotas;
